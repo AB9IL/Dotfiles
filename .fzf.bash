ab6b7d35a5c6ed4fabba84a71da1e267ecb94ec0
Philip Colllier <redcrown01@gmail.com>
2021-02-07 10:54:47 +0000

    Add files via upload

---
+++ .fzf.bash
@@ -0,0 +1,10 @@
+# Setup fzf
+# ---------
+
+# Auto-completion
+# ---------------
+[[ $- == *i* ]] && source "/usr/share/fzf/shell/completion.bash" 2> /dev/null
+
+# Key bindings
+# ------------
+source "/usr/share/fzf/shell/key-bindings.bash"
